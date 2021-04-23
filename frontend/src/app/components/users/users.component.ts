55f8cd056641f2fbc7a66fdba10329ba4902c93d
academy_intern <hastings@thedigitalacademy.co.za>
2021-04-23 14:04:58 +0200

    getting a specific user

--- frontend/src/app/components/users/users.component.ts
+++ frontend/src/app/components/users/users.component.ts
@@ -9,6 +9,7 @@ import { ApiService } from '../../services/api.service';
 })
 export class UsersComponent implements OnInit {
   the_users : any ;
+  user :any;
   constructor(private api : ApiService) { }
 
   ngOnInit(): void {
@@ -19,6 +20,15 @@ export class UsersComponent implements OnInit {
    this.api.getUsers().subscribe((data)=>{
      this.the_users = data;
    })
+ }
+
+ getUser(id){
+   this.api.getSpecificUser(id).subscribe(data=>{
+    this.user = data;
+    console.log(this.user);
+    
+   });
+   
  }
 
 }
