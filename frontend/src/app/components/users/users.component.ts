1eb76f62c5c0c45e2714f4856e3f769317423865
academy_intern <hastings@thedigitalacademy.co.za>
2021-04-23 15:14:03 +0200

    fixed fonts

--- frontend/src/app/components/users/users.component.ts
+++ frontend/src/app/components/users/users.component.ts
@@ -13,15 +13,16 @@ export class UsersComponent implements OnInit {
   constructor(private api : ApiService) { }
 
   ngOnInit(): void {
+    //Call get users on load
     this.getAllUsers(); 
   }
-
+//Get all users from the service 
  getAllUsers(){
    this.api.getUsers().subscribe((data)=>{
      this.the_users = data;
    })
  }
-
+//Request user data from the service
  getUser(id){
    this.api.getSpecificUser(id).subscribe(data=>{
     this.user = data;
--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -9,11 +9,11 @@ export class ApiService {
   theUrl = environment.theUrl;
 
   constructor(private httpClient: HttpClient) {}
-
+//Get all users from the api
   getUsers(){
     return this.httpClient.get(`${this.theUrl}`);
   }
-  
+ //Request specific user from the api
   getSpecificUser(id){
     return this.httpClient.get(`${this.theUrl}/${id}`);
 
